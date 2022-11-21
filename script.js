0f3ec07e07b9d73119ccd7946f659c4f8946df9b
SylasB <sylasboniek@gmail.com>
2022-11-21 12:39:43 -0700

    Added the JS to make it so that it can be played in the Human vs Human mode.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //Try Again
 
 const form = document.querySelector("#inputForm");
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
 
 form.addEventListener('submit', (event) => {
 
@@ -9,6 +19,102 @@ form.addEventListener('submit', (event) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
    document.querySelector(".formWrapper").setAttribute("hidden", true);
+    startGame(data);
 
+});
+
+const startVariables = (data) => {
+    //Game Variables
+        data.choice = +data.choice;
+        data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        data.player1 = "X";
+        data.player2 = "O";
+        data.round = 0;
+        data.currentPlayer = "X";
+        data.gameOver = false;
+};
+
+const startGame = (data) => {
+    startVariables(data);
     console.log(data);
-})
+    //eventListeners
+    addEventListenersToBoard(data);
+    
+    let displayTurnText = data.currentPlayer === "X" ? data.player1Name : data.player2Name; 
+    adjustDom("displayTurn", `${displayTurnText}'s turn`);
+};
+
+const addEventListenersToBoard = (data) => {
+    document.querySelectorAll(".tile").forEach((tile) => {
+        tile.addEventListener('click', (event) => {
+            playMove(event.target, data);
+        })
+    })
+};
+
+const playMove = (tile, data) => {
+    if(data.gameOver || data.round > 8) {
+        return;
+    }
+
+    if(data.board[tile.id] === "X" || data.board[tile.id] === "O") {
+        return;
+    }
+
+    data.board[tile.id] = data.currentPlayer;
+    tile.textContent = data.currentPlayer;
+    tile.className = data.currentPlayer === "X" ? "tile player1" : "tile player2";
+
+    data.round++;
+
+    if(endConditions(data)) {
+        return;
+    }
+
+    changePlayer(data);
+};
+
+const endConditions = (data) => {
+//player wins
+    if(checkWinner(data)) {
+        let winnerName = data.currentPlayer === "X" ? data.player1Name : data.player2Name;
+        adjustDom("displayTurn", winnerName + " has won the game!");
+        data.gameOver = true;
+        return true;
+    }
+//tie
+    else if (data.round === 9) {
+        adjustDom("displayTurn", "It's a Tie!");
+        data.gameOver = true;
+        return true;
+    };
+//not over   
+return false
+};
+
+const checkWinner = (data) => {
+    let result = false;
+    winConditions.forEach(condition => {
+        if(data.board[condition[0]] === data.board[condition[1]] && 
+            data.board[condition[1]] === data.board[condition[2]]
+            ) {
+            console.log('WINNER');
+            data.gameOver = true;
+            result = true;
+        }
+    })
+
+    return result;
+};
+
+const adjustDom = (className, text) => {
+    const elem = document.querySelector(`.${className}`);
+    elem.textContent = text;
+};
+
+const changePlayer = (data) => {
+    data.currentPlayer = data.currentPlayer === "X" ? "O" : "X";
+
+    let displayTurnText = data.currentPlayer === "X" ? data.player1Name : data.player2Name; 
+    adjustDom("displayTurn", `${displayTurnText}'s turn`);
+};
